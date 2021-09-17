96486ccd601cebd56bc60cc59099f288986f77e2
Bricktech <Bricktech2000@gmail.com>
2021-09-17 03:41:44 -0400

    integrated Discord API with Wolfram|Alpha API

--- index.js
+++ index.js
@@ -24,10 +24,13 @@ client.on('ready', () => {
 client.on('messageCreate', (msg) => {
   // https://stackoverflow.com/questions/49663283/how-to-detect-if-the-author-of-a-message-was-a-discord-bot/49667223#:~:text=If%20you%20want%20to%20check%20if%20the%20message%20author%20is,going%20if%20its%20another%20bot.
   if (msg.author.id === client.user.id) return;
+  if (msg.content.split(' ').length < 3) return;
 
-  console.log(msg.content);
-  msg.reply(msg.content);
-  // waApi.getShort(msg.content).then(msg.reply).catch(msg.reply);
+  waApi
+    .getShort(msg.content)
+    .then((res) => msg.reply(res))
+    // .then(console.log)
+    .catch(console.error);
 });
 
 client.login(process.argv[3]);
