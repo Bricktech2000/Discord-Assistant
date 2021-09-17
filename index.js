a7a1a70349fdd4a50b55df196d71eb671c4bcddb
Bricktech <Bricktech2000@gmail.com>
2021-09-17 03:18:15 -0400

    added basic Discord functionality

--- index.js
+++ index.js
@@ -1,18 +1,33 @@
 const { exit } = require('process');
 const WolframAlphaAPI = require('wolfram-alpha-api');
 
+const { Client, Intents } = require('discord.js');
+const client = new Client({
+  intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
+});
+
 if (process.argv.length != 4) {
   console.log('Usage: node . WOLFRAMALPHA_API_KEY DISCORD_BOT_API_KEY');
   exit(1);
 }
 
-console.log('Using WolframAlpha key: ' + process.argv[2]);
-console.log('Using Discord key: ' + process.argv[3]);
+console.log('Using WolframAlpha API key: ' + process.argv[2]);
+console.log('Using Discord bot token: ' + process.argv[3]);
 
 // https://nodejs.org/en/knowledge/command-line/how-to-parse-command-line-arguments/
 const waApi = WolframAlphaAPI(process.argv[2]);
 
-waApi
-  .getShort('what is the mass of the earth')
-  .then(console.log)
-  .catch(console.error);
+client.on('ready', () => {
+  console.log('Bot status: READY');
+});
+
+client.on('messageCreate', (msg) => {
+  // https://stackoverflow.com/questions/49663283/how-to-detect-if-the-author-of-a-message-was-a-discord-bot/49667223#:~:text=If%20you%20want%20to%20check%20if%20the%20message%20author%20is,going%20if%20its%20another%20bot.
+  if (msg.author.id === client.user.id) return;
+
+  console.log(msg.content);
+  msg.reply(msg.content);
+  // waApi.getShort(msg.content).then(msg.reply).catch(msg.reply);
+});
+
+client.login(process.argv[3]);
